83e037642b5d04cbf21d4594537abfb65d81dfa2
Tom Block <ltomblock@gmail.com>
2022-10-12 21:39:09 +0000

    Low

--- index.js
+++ index.js
@@ -144,10 +144,139 @@ for (let i = 0; i < arr.length; i++){
 // return -1 if value not found
 return -1
 }
+/**
+ * function contains: takes an array and returns true if the value exists
+ * and false if not
+ * @param{array} arr: takes an array to loop throuh 
+ * @param{value} val: takes any value to look for in given array
+ */
+// create a contains function that takes in an array and value as args
+function contains(arr, val){
+    // create empty array do push returns into
+    let result;
+    // create loop for arr
+    for (let i = 0; i < arr.length; i++){
+    // use ternary operator to return true if true
+    arr[i] >= val ? result = true : result = false;
+    }
+    return result;
+}
 
+/**
+ * function each: takes a collection and a function. loops through the collection 
+ * running the function on each element or property
+ * @param{array or object} collection: any array or collection
+ * @param{function} func: the function you would like to apply to the looped
+ * collection
+ */
+ function each(collection, func){
+    //test if collection is a array
+    if(Array.isArray(collection)){
+    //call input function on each ellement of array
+      for (let i = 0; i < collection.length; i++){
+        func(collection[i], i, collection);
+      }
+    }
+// else for object
+    else{
+        //invoke input func of each key in object
+        for(let key in collection){
+            //invoke function
+            func(collection[key], key, collection);
+        }
+    }
+}
+/**
+ * function unique: takes an array and returns a new array with all duplicates removed
+ * @param{array} array: the orginal array to loop through and find dupes
+ * 
+ */
+ function unique(array){
+    let newArr = [];
+    let testObject = {};
+    //create loop to take an element from test array
+    for (let i = 0; i < array.length; i++){
+        // create test to see if array element alreay exists in object, if not add to object
+        // and newArr
+        if(array[i] in testObject === false){
+            testObject[array[i]] = 'true';
+            newArr.push(array[i]);
+        }
+    }  
+    //return new array
+    console.log(testObject)
+      return newArr;
+    }
+/**
+ * function filter: takes a array and for each element runs a function, if the
+ * function returns true the element is pushed into a new array and that new array
+ * is returned
+ * @param{array} arr: the array to loop through
+ * @param{function} func: the function to preform
+ */
+ function filter(arr, func){
+    // create a new array of elements to return
+let newArr = [];
+//call <function> for each element in <array> passing the arguments:
+//      the element, it's index, <array> for loop to go through array
+for (let i = 0; i < arr.length; i++){
+   if (func(arr[i], i, arr)){
+    // return a new array of elements for which calling <function> returned true
+    newArr.push(arr[i]);
+   }
+ }
+    //return new arr 
+   return newArr;
+}
+/**
+ * function reject: takes an array and a function looping through array and running
+ * the function on each element. If the function returns false the element is pushed
+ * into a new array.  The new array is returned
+ * @param{array} arr: the array to loop through
+ * @param{function} func: the function to run on each element in array
+ */
+function reject(arr, func){
+    // create empty output array
+    let newArr = [];
+//call <function> for each element in <array> passing the arguments:
+//      the element, it's index, <array> for loop to go through array
+for (let i = 0; i < arr.length; i++){
+    if (func(arr[i], i, arr) === false){
+     // return a new array of elements for which calling <function> returned false
+     newArr.push(arr[i]);
+ }
+  }
+     //return new arr 
+    return newArr;
+ }
+/**
+ * function partition: takes an array and an function, loops through the array
+ * when the provided function returns true it puts the element in one subarray
+ * and when false puts element in other subaray. Returns new 2-d array
+ * @param{array} arr: takes the array to partition
+ * @param{function} func: the function to run on the elements in array
+ * */
+function partition(arr, func){
+    // create empty output nested array
+    let newArr = [[],[]];
+//call <function> for each element in <array> passing the arguments:
+//      the element, it's index, <array> for loop to go through array
+for (let i = 0; i < arr.length; i++){
+    // if true
+    if (func(arr[i], i, arr)){
+     // return a new array of elements for which calling <function> returned false
+     newArr[0].push(arr[i]);
+ }
+  //if false
+  else {
+    newArr[1].push(arr[i]);
+  }
+}
+     //return new arr 
+    return newArr;
+ }
 
-
-
+ 
 
 
 module.exports.each = each;
